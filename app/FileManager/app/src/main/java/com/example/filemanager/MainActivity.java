507365fd1c7c58efe72d89566ef23fa76d3a1976
Jasn9 <jatin9gupta@gmail.com>
2020-10-23 14:05:06 +0530

    epsiode1

--- app/FileManager/app/src/main/java/com/example/filemanager/MainActivity.java
+++ app/FileManager/app/src/main/java/com/example/filemanager/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.filemanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout1);
+
+        final Button b1 = findViewById(R.id.b1);
+        final Button b2 = findViewById(R.id.b2);
+        final Button b3 = findViewById(R.id.b3);
+        final Button b4 = findViewById(R.id.b4);
+        final Button b5 = findViewById(R.id.b5);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b1.setText("pressed");
+            }
+        });
+
+        final ListView listView = findViewById(R.id.listView);
     }
 }
