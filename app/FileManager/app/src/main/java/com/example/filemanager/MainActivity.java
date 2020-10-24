a0d04130478df4f1dc5d3d8cdff0ea404666bd05
Jasn9 <jatin9gupta@gmail.com>
2020-10-24 16:51:51 +0530

    episode3

--- app/FileManager/app/src/main/java/com/example/filemanager/MainActivity.java
+++ app/FileManager/app/src/main/java/com/example/filemanager/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.filemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.ActivityManager;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +18,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,21 +45,21 @@ public class MainActivity extends AppCompatActivity {
         listView.setAdapter(textAdapter1);
 
         List<String> example = new ArrayList<>();
-        for(int i=0;i<100;i++){
+        for (int i = 0; i < 100; i++) {
             example.add(String.valueOf(i));
         }
 
         textAdapter1.setData(example);
     }
 
-    class TextAdapter extends BaseAdapter{
+    class TextAdapter extends BaseAdapter {
 
         private List<String> data = new ArrayList<>();
 
-        public void setData(List<String> data){
-            if(data!=null){
+        public void setData(List<String> data) {
+            if (data != null) {
                 this.data.clear();
-                if(!data.isEmpty()){
+                if (!data.isEmpty()) {
                     this.data.addAll(data);
                 }
                 notifyDataSetChanged();
@@ -77,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            if(convertView==null){
+            if (convertView == null) {
                 convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
                 convertView.setTag(new ViewHolder(convertView.findViewById(R.id.textItem)));
             }
@@ -89,10 +95,52 @@ public class MainActivity extends AppCompatActivity {
 
         class ViewHolder {
             TextView info;
-            ViewHolder(TextView info){
+
+            ViewHolder(TextView info) {
                 this.info = info;
             }
         }
     }
 
+    private static final int REQUEST_PERMISSIONS = 1234;
+
+    private static final String[] PERMISSIONS = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    private static final int PERMISSIONS_COUNT = 2;
+
+    @SuppressLint("NewApi")
+    private boolean arePermissionsDenied(){
+        for (String permission : PERMISSIONS) {
+                if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                    return true;
+                }
+        }
+        return false;
+    }
+
+    // to check if we still have permission, if user disabled permission later
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (arePermissionsDenied()) {
+                requestPermissions(PERMISSIONS, REQUEST_PERMISSIONS);
+            }
+        }
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    public void onRequestPermissionsResult(final int requestCode, final String[] permissions, final int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==REQUEST_PERMISSIONS && grantResults.length > 0){
+            if(arePermissionsDenied()){
+                ((ActivityManager) Objects.requireNonNull(this.getSystemService(ACTIVITY_SERVICE))).clearApplicationUserData();
+                recreate();
+            }
+        }
+    }
 }
