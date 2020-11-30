f0b51f19d08d186588289bfd262e81f12da11319
Rishabh Pahwa <37842938+underworld123@users.noreply.github.com>
2020-11-30 17:27:25 +0530

    Update routers.js

--- backend/routes/routers.js
+++ backend/routes/routers.js
@@ -13,35 +13,82 @@ function generateCode(id){
     }
     return id+res;
 }
-
 //get Token for Authentication
 app.post('/getCode', async (req,res) => {
-	console.log("In getCode"+req.body.id);
-	if (!req.body.id) {
+    console.log("In getCode"+req.body.id);
+    if (!req.body.id) {
         res.status(400).send({ message: "Request should contain field id!" });
         return;
     }
-    //check for code reduntancy and then return unique code
-    var token=generateCode(req.body.id);
-    const temp_auth=await authModel.findOne({Token:token})
-    while(temp_auth!=null){
-    	token=generateCode(req.body.id);
-    	temp_auth=await authModel.findOne({Token:token})
-    }
-    var id=req.body.id;
-    console.log("token : "+token+' id : '+id);
-    const json_obj={
-    	'Token':token,
-    	'AndroidId':id,
-    	'ClientId':'temp'
-    }
-    const auth = new authModel(json_obj);
-    try {
-        await auth.save();
-        res.send({'code':token});
-    } catch (err) {
-        console.log(err);
-        res.status(500).send({message:"Cannot get the code write now please try again"});
+    var anid=req.body.id;
+    try{
+        authModel.findOne({AndroidId:anid}, async function(err,data){
+            if(err||data==null){
+                //check for code redundancy and then return unique code
+                var token=generateCode(req.body.id);
+                const temp_auth=await authModel.findOne({Token:token})
+                while(temp_auth!=null){
+                    token=generateCode(req.body.id);
+                    temp_auth=await authModel.findOne({Token:token})
+                }
+                var id=req.body.id;
+                console.log("token : "+token+' id : '+id);
+                const json_obj={
+                    'Token':token,
+                    'AndroidId':id,
+                    'ClientId':'temp',
+                    'TimeStamp':new Date()
+                }
+                const auth = new authModel(json_obj);
+                try {
+                    await auth.save();
+                    res.send({'code':token});
+                } catch (err) {
+                    console.log(err);
+                    res.status(500).send({message:"Cannot get the code write now please try again"});
+                }  
+            }
+            else{
+                var initDate=data['TimeStamp']; var curDate=new Date();
+                var minutes = (curDate.getTime() - initDate.getTime()) / (1000*60);
+                if(minutes>30){
+                    authModel.deleteOne({Token:data['Token']},async (err)=>{
+                        if(err){
+                            console.log("Inside getCode deleteOne error : "+err)
+                        }
+                        console.log("Inside getCode, Deleted the old record")
+                    })
+                    var token=generateCode(req.body.id);
+                    const temp_auth=await authModel.findOne({Token:token})
+                    while(temp_auth!=null){
+                        token=generateCode(req.body.id);
+                        temp_auth=await authModel.findOne({Token:token})
+                    }
+                    var id=req.body.id;
+                    console.log("token : "+token+' id : '+id);
+                    const json_obj={
+                        'Token':token,
+                        'AndroidId':id,
+                        'ClientId':'temp',
+                        'TimeStamp':new Date()
+                    }
+                    const auth = new authModel(json_obj);
+                    try {
+                        await auth.save();
+                        res.send({'code':token});
+                    } catch (err) {
+                        console.log(err);
+                        res.status(500).send({message:"Cannot get the code write now please try again"});
+                    }  
+                }
+                else{
+                    var token=data['Token'];
+                    res.status(200).send({'code':token});    
+                }
+            }
+        });
+    }catch(err){
+        console.log("In getcode error: "+err);
     }
 });
 
@@ -54,8 +101,39 @@ app.get('/showAll', async (req, res) => {
   }
 });
 
+app.get('/cleanDB', async (req,res) => {
+    if(!req.body.password){
+        res.status(400).send({message:"Request should contain field password!"});
+    }
+    var password=req.body.password;
+    if(password==="admin@123"){
+        const auths = await authModel.find({});
+        try{
+            for(var i=0;i<auths.length;i++){
+                var cur_record=auths[i]
+                var initDate=auths[i]['TimeStamp']; var curDate=new Date();
+                var minutes = (curDate.getTime() - initDate.getTime()) / (1000*60);
+                if(minutes>30){
+                    authModel.deleteOne({Token:auths[i]['Token']},async (err)=>{
+                        if(err){
+                            console.log("Inside ClearDb deleteOne error : "+err)
+                        }
+                        console.log("Deleted this record : "+cur_record+" "+minutes)
+                    })
+                }
+            }
+            res.status(200).send("OK");
+        }catch(err){
+            console.log("Inside ClearDb API error: "+err);
+        }
+    }
+    else{
+        res.status(400).send({message:"Wrong password!"});
+    }
+})
+
 app.post('/verifyCode', async (req,res) => {
-	if((!req.body.id)||(!req.body.code)){
+    if((!req.body.id)||(!req.body.code)){
         res.status(400).send({message:"Request should contain field id and code!"});
     }
     var clientId=req.body.id;
@@ -64,38 +142,38 @@ app.post('/verifyCode', async (req,res) => {
     console.log("In verifyCode api = "+clientId+" "+Code);
 
     authModel.findOne({Token:Code}, async function(err,data){
-    	if(err){
-    		console.log(err);
-    		res.status(400).send({message:'No such Code Found'});
-    	}
-    	console.log(data)
-    	androidid=data['AndroidId'];
-    	var updatedauth={
-    		'Token':data['Token'],
-    		'AndroidId':androidid,
-    		'ClientId':clientId
-    	}
-    	await authModel.findByIdAndUpdate(data['_id'], updatedauth);
-    	// await authModel.save()
-    	console.log(updatedauth);
-    	res.status(200).send({message:'Code Verfied Successfully'})
+        if(err){
+            console.log(err);
+            res.status(400).send({message:'No such Code Found'});
+        }
+        console.log(data)
+        androidid=data['AndroidId'];
+        var updatedauth={
+            'Token':data['Token'],
+            'AndroidId':androidid,
+            'ClientId':clientId
+        }
+        await authModel.findByIdAndUpdate(data['_id'], updatedauth);
+        // await authModel.save()
+        console.log(updatedauth);
+        res.status(200).send({message:'Code Verfied Successfully'})
     });
 });
 
 app.post('/logout', async (req,res) => {
-	if(!req.body.code){
+    if(!req.body.code){
         res.status(400).send({message:"Request should contain field code"});
     }
     var Code=req.body.code;
     console.log('In logout code = '+Code);
     authModel.findOne({Token:Code}, async function(err,data){
-    	if(err){
-    		console.log(err);
-    		res.status(404).send({message:'No such Code Found'});
-    	}
-    	const dlt_auth=await authModel.findByIdAndDelete(data['id'])
-    	if(!dlt_auth)res.status(404).send("No item found")
-    	else res.status(200).send({message:'Logout Successfully'});
+        if(err){
+            console.log(err);
+            res.status(404).send({message:'No such Code Found'});
+        }
+        const dlt_auth=await authModel.findByIdAndDelete(data['id'])
+        if(!dlt_auth)res.status(404).send("No item found")
+        else res.status(200).send({message:'Logout Successfully'});
     })
 })
 module.exports = app
